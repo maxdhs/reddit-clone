141986ddf0e6ec292ae230f250448735d84ccfe8
Maxwell Dubin <maxdhs@gmail.com>
2023-12-27 13:41:07 -0500

    ...

---
+++ prisma/migrations/20231227181052_/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Subreddit` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Subreddit" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Subreddit" ADD CONSTRAINT "Subreddit_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/subreddits/[subredditId]/page.jsx
+++ src/app/subreddits/[subredditId]/page.jsx
@@ -1,7 +1,24 @@
-export default function Subreddit({ params }) {
+import { prisma } from "@/lib/prisma.js";
+
+export default async function Subreddit({ params }) {
   // i want to show the posts associated with this subreddit
 
   // how do i access that parameter?
-  console.log(params);
-  return <div>Subreddit</div>;
+
+  // subreddit name as a title
+  const { subredditId } = params;
+  const subreddit = await prisma.subreddit.findFirst({
+    where: { id: subredditId },
+  });
+
+  const posts = await prisma.post.findMany({ where: { subredditId } });
+  console.log(posts);
+  return (
+    <div>
+      <h3>{subreddit.name}</h3>
+      {posts.map((post) => (
+        <div>{post.title}</div>
+      ))}
+    </div>
+  );
 }
